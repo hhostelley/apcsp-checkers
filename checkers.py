a59d1c7e1f45a01b6c7d96188025467029f726cb
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-11-16 12:46:26 -0700

    Add code to insert image in pygame and create assets folder with start image inside

--- checkers.py
+++ checkers.py
@@ -1,4 +1,21 @@
 import sys, pygame
+from pygame.locals import *
+import os
+
+## PYGAME and Load_Image function are imported code that we do not take credit for
+def load_image(name, colorkey=None):
+    fullname = os.path.join('assets', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
 
 class CheckersMain:
     def __init__(self, width=640,height=480):
@@ -8,11 +25,24 @@ class CheckersMain:
         self.screen = pygame.display.set_mode((self.width, self.height))
 
     def MainLoop(self):
+        self.LoadSprites();
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                    
+            self.snake_sprites.draw(self.screen)
+            pygame.display.flip()
+
+    def LoadSprites(self):
+        self.snake = Snake()
+        self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
+
+class Snake(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('start.png',-1)
+        self.pellets = 0
+
 if __name__ == "__main__":
     MainWindow = CheckersMain()
     MainWindow.MainLoop()
