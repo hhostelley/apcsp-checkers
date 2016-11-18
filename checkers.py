923a3d4f864bbe4f6e6409e4a5eb75103349a0d2
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-11-18 12:07:42 -0700

    Simplify code and add background image

--- checkers.py
+++ checkers.py
@@ -12,15 +12,18 @@ class CheckersMain:
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.start_image = pygame.image.load("assets/start.png")
         self.title1_image = pygame.image.load("assets/title1.png")
+        self.bg = pygame.image.load("assets/bg.png")
 
     def MainLoop(self):
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.blit(self.bg, (0, 0))
             self.screen.blit(self.start_image, (355, 450))
             self.screen.blit(self.title1_image, (240, 10))
             pygame.display.flip()
+
 
 
 if __name__ == "__main__":
