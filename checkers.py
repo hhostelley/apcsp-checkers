925fabe72189f785bdf52af4709136fe6be60a76
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-11-18 12:39:48 -0700

    Rewrite code to be simpler, redesigned start button

--- checkers.py
+++ checkers.py
@@ -1,31 +1,51 @@
 import sys, pygame
-from pygame.locals import *
 import os
+from pygame.locals import *
 
-## PYGAME imported
+bg = pygame.image.load('assets/bg.png')
+title1 = pygame.image.load('assets/title1.png')
 
-class CheckersMain:
-    def __init__(self, width=1280,height=720):
-        pygame.init()
-        self.width = width
-        self.height = height
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        self.start_image = pygame.image.load("assets/start.png")
-        self.title1_image = pygame.image.load("assets/title1.png")
-        self.bg = pygame.image.load("assets/bg.png")
+class Option:
 
-    def MainLoop(self):
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.blit(self.bg, (0, 0))
-            self.screen.blit(self.start_image, (355, 450))
-            self.screen.blit(self.title1_image, (240, 10))
-            pygame.display.flip()
+    hovered = False
 
+    def __init__(self, text, pos):
+        self.text = text
+        self.pos = pos
+        self.set_rect()
+        self.draw()
 
+    def draw(self):
+        self.set_rend()
+        screen.blit(self.rend, self.rect)
 
-if __name__ == "__main__":
-    MainWindow = CheckersMain()
-    MainWindow.MainLoop()
+    def set_rend(self):
+        self.rend = menu_font.render(self.text, True, self.get_color())
+
+    def get_color(self):
+        if self.hovered:
+            return (255, 255, 255)
+        else:
+            return (100, 100, 100)
+
+    def set_rect(self):
+        self.set_rend()
+        self.rect = self.rend.get_rect()
+        self.rect.topleft = self.pos
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+menu_font = pygame.font.Font(None, 180)
+options = [Option("START", (400, 400))]
+while True:
+    pygame.event.pump()
+    screen.fill((0, 0, 0))
+    screen.blit(bg,(0,0))
+    screen.blit(title1,(240,10))
+    for option in options:
+        if option.rect.collidepoint(pygame.mouse.get_pos()):
+            option.hovered = True
+        else:
+            option.hovered = False
+        option.draw()
+    pygame.display.update()
